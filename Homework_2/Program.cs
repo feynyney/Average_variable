fed2129849b34e3c49600ad3301d3bbd634cf34e
feynyney <feynyney@gmail.com>
2022-04-04 19:42:21 +0300

    Work is done

---
+++ Homework_2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Homework_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Lapenko Oleg Vladimirovich";
+
+            int age = 23;
+
+            string email = "lapenkoleg@gmail.com";
+
+            float itPoints = 4.25f;
+
+            float mathPoints = 3.75f;
+
+            float physPoints = 5;
+
+            string output = "Name: {0} \nAge: {1} \nEmail: {2} \nIT points: {3} \nMath points: {4} \nPhysics points: {5}";
+
+            Console.WriteLine(output , name , age , email , itPoints , mathPoints , physPoints);
+
+            float Sum= itPoints + mathPoints + physPoints;
+
+            float Avg = Sum / 3;
+
+            Console.WriteLine($"Average number is {Avg}");
+        }
+    }
+}
